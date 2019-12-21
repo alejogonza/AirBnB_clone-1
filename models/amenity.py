1f3ab215125d12c9bec34ef7dddd5a0b9b34ee81
zelda096 <miguelangel697@gmail.com>
2019-12-20 23:31:32 -0500

    fix

--- models/amenity.py
+++ models/amenity.py
@@ -1,23 +1,18 @@
-#!/usr/bin/python
-"""
-Class Amenity
-"""
-import models
+#!/usr/bin/python3
+"""This is the amenity class"""
 from models.base_model import BaseModel, Base
-from os import getenv
-import sqlalchemy
-from sqlalchemy import Column, String
+from models.place import place_amenity
+from sqlalchemy import Column, Integer, String, ForeignKey
 from sqlalchemy.orm import relationship
+from os import getenv
 
 
 class Amenity(BaseModel, Base):
-    """The Amenity class"""
-    if models.storage_t == 'db':
-        __tablename__ = 'amenities'
-        name = Column(String(128), nullable=False)
-    else:
-        name = ""
-
-    def __init__(self, *args, **kwargs):
-        """Constructor"""
-        super().__init__(*args, **kwargs)
+    """This is the class for Amenity
+    Attributes:
+        name: input name
+    """
+    __tablename__ = 'amenities'
+    name = Column(String(128), nullable=False)
+    if getenv('HBNB_TYPE_STORAGE') == 'db':
+        place_amenities = relationship('Place', secondary=place_amenity)
