cd5249b3bcfd7c1f88bd3c606e8b4792b9430fd1
alejogonza <alejo.1996.2001@gmail.com>
2020-01-04 01:10:18 +0000

    [feature] add data

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,19 +1,22 @@
 #!/usr/bin/env bash
+# Sets up web server for deploy
 
 sudo apt-get -y update
 sudo apt-get -y install nginx
-sudo sed -i "15i add_header X-Served-By \$hostname;" /etc/nginx/nginx.conf
-mkdir -p /data/web_static/{releases/test,shared}
-echo "<html>
-<head>
-</head>
+mkdir -p /data/web_static/shared
+mkdir -p /data/web_static/releases/test
+touch /data/web_static/releases/test/index.html
+echo -e "
+<html>
+  <head>
+  </head>
   <body>
     Holberton School
   </body>
-</html>" > /data/web_static/releases/test/index.html
-sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
-sudo chown -R ubuntu:ubuntu /data/
-sed -i '/listen 80 default_server/a location /hbnb_static/ { alias /data/web_static/current/;}' /etc/nginx/sites-available/default
+</html>
+" | sudo tee -a /data/web_static/releases/test/index.html
+ln -fs /data/web_static/releases/test /data/web_static/current
+chown -R ubuntu /data
+chgrp -R ubuntu /data
+sed -i "33i\ \n\tlocation /hbnb_static/\n\t{\n\t\talias /data/web_static/current/;\n\t}\n" /etc/nginx/sites-available/default
 sudo service nginx restart
-exit 0
-
